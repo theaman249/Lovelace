6ad01ef2c1af59781aa2f3b0a7461b1d43addd09
Agape <theaman249@gmail.com>
2024-08-26 02:25:13 +0200

    ✅ Written the first database query

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,52 @@
 const express = require('express');
+const cors = require('cors');
+const db = require('./conn'); //import the database module
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('The Lovelace server is running!');
+
+  const message ={
+    "msg": "Lovelace backend is running"
+  };
+
+  res.send(JSON.stringify(message));
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+});
+
+
+
+/////////////////////////DB API FUNCTIONS/////////////////////////////////
+
+/**
+ * This function returns all employees from the database as a JSON array
+ * that contains json employee objects.
+*/
+app.get('/getAllEmployees', async (req,res) =>{
+
+  const query = 'SELECT * FROM employees';
+  const errorMessage = 'employees relation not found.';
+
+  try {
+    const { rows } = await db.query(query);
+
+    if (rows.length === 0) {
+      res.status(404).json({ error: errorMessage });
+    } 
+    else {
+      res.json(rows);
+    }
+  } catch (error) {
+    console.error('Error querying the database:', error);
+    res.status(500).send('Internal Server Error');
+  }
+
 });
 
 
