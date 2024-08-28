43a6897428a129dd06fd6a49db096d33a24b6a5f
Amanda9805 <akhuzwayo98@gmail.com>
2024-08-28 20:00:45 +0200

    employee model

---
+++ backend/models/employee.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../seq_init'); // Assuming db.js exports the Sequelize instance
+
+
+const Employee = sequelize.define('employees', {
+   name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  surname: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone_number: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  birthday: {
+    type: DataTypes.DATE,
+    allowNull: false
+},
+}, {
+  timestamps: false,
+  createdAt: false,
+  updatedAt: false
+});
+
+
+module.exports = Employee;
