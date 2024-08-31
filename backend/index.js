386fc4fc4280b7d929a6942da416ccf18624f904
Amanda9805 <akhuzwayo98@gmail.com>
2024-08-31 19:01:18 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const db = require('./conn'); //import the database module
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const adminRoutes = require('./routes/admin');
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/auth', authRoutes); // Mount routes
 app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
 
 app.get('/', (req, res) => {
 
