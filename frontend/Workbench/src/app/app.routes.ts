99ce9ac31ee0d5994684b92cfdbe05f2b194615f
TheStoryOfCampion <94606154+TheStoryOfCampion@users.noreply.github.com>
2024-08-31 09:34:02 +0200

    Sisempini ke manje

--- frontend/Workbench/src/app/app.routes.ts
+++ frontend/Workbench/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { OfficeBookingsComponent } from './office-bookings/office-bookings.component';
+// import { OfficeBookingsComponent } from './office-bookings/office-bookings.component';
 
 const routeConfig: Routes = [];
 
