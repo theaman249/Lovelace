72bfcd770952874297e9634298540a9213ade441
TheStoryOfCampion <94606154+TheStoryOfCampion@users.noreply.github.com>
2024-08-31 08:53:15 +0200

    Yohh yohh yohh

--- frontend/Workbench/src/app/app.routes.ts
+++ frontend/Workbench/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { OfficeBookingsComponent } from './office-bookings/office-bookings.component';
+
+const routeConfig: Routes = [];
+
+export default routeConfig;
 
 export const routes: Routes = [];
