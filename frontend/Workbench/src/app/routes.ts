172423acc657cd4d1148618cdbc8da91b1d1da92
TheStoryOfCampion <94606154+TheStoryOfCampion@users.noreply.github.com>
2024-08-31 18:45:22 +0200

    YES YES YES

--- frontend/Workbench/src/app/routes.ts
+++ frontend/Workbench/src/app/routes.ts
@@ -1,12 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { OfficeBookingsComponent } from './office-bookings/office-bookings.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
     {
         path: '',
         component:LoginComponent,
+        title: 'Login'
+    },
+    {
+        path: 'Home',
+        component: HomeComponent,
         title: 'Home Page'
+    },
+    {
+        path: 'Signup',
+        component: SignupComponent,
+        title: 'Sign Up'
+    },
+    {
+        path: 'Office',
+        component: OfficeBookingsComponent,
+        title: 'Office Bookings'
+    },
+    {
+        path: 'Profile',
+        component: ProfilePageComponent,
+        title: 'Profile Page'
+    },
+    {
+        path: 'Dashboard',
+        component: StatisticsComponent,
+        title: 'Dashboard'
+    },
+    {
+        path: 'Forgot',
+        component: ForgotPasswordComponent,
+        title:'Forgot Password'
     }
 ];
 
