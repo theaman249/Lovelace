5269c38f97a54fc4050fea74c8efcd256a781a60
Agape <theaman249@gmail.com>
2024-09-01 06:48:12 +0200

    ➕ Added main title to Home Page and Stock images

--- frontend/Workbench/src/app/home/home.component.ts
+++ frontend/Workbench/src/app/home/home.component.ts
@@ -12,8 +12,27 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  mainTitle = 'welcome User';
+
   ngOnInit(): void {
-      
+    const name = this.getCookie('name');
+    this.mainTitle ='welcome '+name;
+  }
+
+  getCookie(name: string): string | null {
+    const nameEQ = `${name}=`;
+    const cookies = document.cookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i];
+      while (cookie.charAt(0) == ' ') {
+        cookie = cookie.substring(1);
+      }
+      if (cookie.indexOf(nameEQ) == 0) {
+        return cookie.substring(nameEQ.length, cookie.length);
+      }
+    }
+    return null;
   }
 
 }
