4637d1a69d3d9ad6c7919e7ba70f72263c03b9c1
Agape <theaman249@gmail.com>
2024-09-01 04:16:28 +0200

    ⚡️ Initial data harvesting done on registration page.

--- frontend/Workbench/src/app/auth.service.ts
+++ frontend/Workbench/src/app/auth.service.ts
@@ -13,6 +13,7 @@ export class AuthService {
 
   public loginUser(email:string,password:string):Promise<User>{ 
     const url = 'http://localhost:3000/auth/login';
+    
     const body = {
       email:email,
       password: password
@@ -35,6 +36,36 @@ export class AuthService {
       throw error; // Re-throw the error after logging or handling it
     });
 
+  }
+
+  public signupUser(name:string,surname:string,email:string,password:string,role:string):Promise<User>
+  {
+    const url = 'http://localhost:3000/auth/register';
+    
+    const body = {
+      name:email,
+      surname: surname,
+      email:email,
+      password:password,
+      role:role,
+    }
+
+    return this.http.post<any>(url, body).toPromise()
+    .then(res => {
+      return new User(
+        res.name,
+        res.surname,
+        res.email,
+        res.role,
+        res.phone_number,
+        res.birthday,
+        res.message
+      );
+    })
+    .catch(error => {
+      console.error('Error during login:', error);
+      throw error; // Re-throw the error after logging or handling it
+    });
   }
 
 
